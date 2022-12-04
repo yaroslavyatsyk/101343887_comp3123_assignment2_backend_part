b9fc100d8630724acbb7e442e0278e2c96f02c4b
Yaroslav9728 <yaroslav9728@gmail.com>
2022-12-04 18:31:42 -0500

    updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 
 app.use('/api/user',userRouter)
-app.use('/api/emp',employeeRouter)
+app.use('/api/emp/employees',employeeRouter)
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
