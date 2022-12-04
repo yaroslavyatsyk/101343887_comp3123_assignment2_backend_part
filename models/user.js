050b08119a7a5314a6f9ee739f67ced04b5cf339
Yaroslav9728 <yaroslav9728@gmail.com>
2022-12-04 18:38:28 -0500

    many updates

--- models/user.js
+++ models/user.js
@@ -6,17 +6,25 @@ const userSchema = new mongoose.Schema({
     username : {
         type: String,
         unique: true,
-        index: true
+        index: true,
+        required: true
     },
     email : {
         type: String,
         unique: true,
+        required: true,
         validate(value) {
-			return !RegExpression.forValidEmail.test(value)
+			return !/RegExpression.forValidEmail/.test(value)
 		}
+        
     },
     password : {
         type: String,
+        required: true,
+        validate(value) {
+			return !/RegExpression.forStrongPassword/.test(value)
+		},
+        info: props => `${props.value} is not a strong password`
     }
     
 },
