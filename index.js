5ccc03d4e49bdf81415319f4a29d9edb8fb9485d
Yaroslav9728 <yaroslav9728@gmail.com>
2022-12-04 21:44:44 -0500

    updated

--- index.js
+++ index.js
@@ -11,13 +11,6 @@ app.use('/api/emp/employees',employeeRouter)
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
 
 mongoose.Promise = global.Promise
 
